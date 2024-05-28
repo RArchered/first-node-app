7861621478012aa0591e56c159719effe1a63a9a
jiaxinghe <jiaxinghe@tencent.com>
2024-05-28 17:17:28 +0800

    first-node-app created

---
+++ http_server.js
@@ -0,0 +1,42 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+http.createServer(function (req, res) {
+  // 6666端口浏览器无法访问
+  const url = req.url;
+  let fpath = '';
+  console.log("url: " + url);
+  if (url === '/flower') {
+    // 展示烟花
+    fpath = path.join(__dirname, './index.html')
+  } else if (url.startsWith('/demo')) {
+    // 展示demo页面
+    fpath = path.join(__dirname, './demo-html-css', url.substring(5))
+  } else if (url.startsWith('/first_react')) {
+    // 展示第一个react页面
+    fpath = path.join(__dirname, './first-react-app/build', url.substring(12))
+  }
+  console.log("fpath: " + fpath);
+  if (fpath === '') {
+    return res.end('404 Not found');
+  }
+
+  // 图片文件设置content-type
+  const fileExtension = path.extname(fpath).toLowerCase();
+  if (['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg', '.ico'].includes(fileExtension)) {
+    res.setHeader('Content-type','image/*')
+  }
+  if (['.svg'].includes(fileExtension)) {
+    res.setHeader('Content-type','image/svg+xml')
+  }
+  // 以字节形式读取
+  fs.readFile(fpath, (err, dataBuffer) => {
+    if (err) {
+        res.statusCode = 404
+        return res.end('404 Not found')
+    }
+    res.write(dataBuffer)
+    res.end()
+  })
+}).listen(8888, '0.0.0.0');
+console.log('Server running at http://0.0.0.0:8888/');
